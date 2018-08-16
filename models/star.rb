b6e7007c961496630046fdc188223ab124448e38
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-16 14:34:34 +0100

    Add sql tables, star class initialize

--- models/star.rb
+++ models/star.rb
@@ -1,3 +1,13 @@
+require_relative('../db/sql_runner.rb')
+
 class Star
+  attr_accessor :title, :genre
+  attr_reader :id
+
+  def intialize(info)
+    @id = info['id'].to_i if info['id']
+    @title = info['title']
+    @genre = info['genre']
+  end
 
 end
