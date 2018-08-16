ec55903e5c04c35182c9bb920df39aa17fb7bdd7
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-16 14:38:54 +0100

    Add movies to console

---
+++ console.rb
@@ -0,0 +1,8 @@
+movie1 = Movie.new({
+  'title' => 'Bourne Identity'
+  'genre' => 'thriller'
+  })
+movie2 = Movie.new({
+  'title' => 'Bourne Ultimatum'
+  'genre' => 'thriller'
+  })
--- models/movie.rb
+++ models/movie.rb
@@ -1,3 +1,12 @@
 class Movie
 
+  attr_accessor :title, :genre
+  attr_reader :id
+
+  def intialize(info)
+    @id = info['id'].to_i if info['id']
+    @title = info['title']
+    @genre = info['genre']
+  end
+
 end
