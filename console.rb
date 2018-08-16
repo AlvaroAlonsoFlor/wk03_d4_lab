bbb41bd717392d6b91f70c93d1b418158866b155
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-16 14:55:22 +0100

    Add save and all methods to movie

--- console.rb
+++ console.rb
@@ -1,8 +1,15 @@
+require('pry-byebug')
+require_relative('models/movie')
+
+
 movie1 = Movie.new({
-  'title' => 'Bourne Identity'
+  'title' => 'Bourne Identity',
   'genre' => 'thriller'
   })
 movie2 = Movie.new({
-  'title' => 'Bourne Ultimatum'
+  'title' => 'Bourne Ultimatum',
   'genre' => 'thriller'
   })
+
+binding.pry
+nil
--- models/movie.rb
+++ models/movie.rb
@@ -1,12 +1,30 @@
+require_relative('../db/sql_runner.rb')
+
 class Movie
 
   attr_accessor :title, :genre
   attr_reader :id
 
-  def intialize(info)
+  def initialize(info)
     @id = info['id'].to_i if info['id']
     @title = info['title']
     @genre = info['genre']
+  end
+
+  def save()
+    sql = "INSERT INTO movies
+    (title, genre)
+    VALUES
+    ($1, $2) RETURNING *"
+    values = [@title, @genre]
+    result = SqlRunner.run(sql, values)
+    @id = result.first['id'].to_i
+  end
+
+  def self.all
+    sql = "SELECT * FROM movies"
+    movies = SqlRunner.run(sql)
+    return movies.map { |movie| Movie.new(movie)  }
   end
 
 end
