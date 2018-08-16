063cc62bfe09754856ac156092172a10f3885085
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-16 15:42:42 +0100

    Add save method to star

--- console.rb
+++ console.rb
@@ -1,5 +1,6 @@
 require('pry-byebug')
 require_relative('models/movie')
+require_relative('models/star')
 
 
 movie1 = Movie.new({
@@ -9,6 +10,12 @@ movie1 = Movie.new({
 movie2 = Movie.new({
   'title' => 'Bourne Ultimatum',
   'genre' => 'thriller'
+  })
+
+
+star1 = Star.new({
+  'first_name' => 'Matt',
+  'last_name' => 'Damon'
   })
 
 binding.pry
--- models/star.rb
+++ models/star.rb
@@ -1,5 +1,25 @@
 require_relative('../db/sql_runner.rb')
 
 class Star
-  
+
+  attr_reader :id
+  attr_accessor :first_name, :last_name
+
+  def initialize(info)
+    @id = info['id'].to_i if info['id']
+    @first_name = info['first_name']
+    @last_name = info['last_name']
+  end
+
+  def save
+    sql = "INSERT INTO stars
+    (first_name, last_name)
+    VALUES
+    ($1, $2)
+    RETURNING *"
+    values = [@first_name, @last_name]
+    result = SqlRunner.run(sql, values)
+    @id = result.first['id'].to_i
+  end
+
 end
