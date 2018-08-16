32ebad046a892dd3e49475021ff8f4d441e4a9b2
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-16 15:23:57 +0100

    Add update method to movie

--- models/movie.rb
+++ models/movie.rb
@@ -21,10 +21,38 @@ class Movie
     @id = result.first['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE movies
+    SET title = $1, genre = $2
+    WHERE id = $3 "
+    values = [@title, @genre, @id]
+    SqlRunner.run(sql, values)
+
+  end
+
   def self.all
     sql = "SELECT * FROM movies"
     movies = SqlRunner.run(sql)
-    return movies.map { |movie| Movie.new(movie)  }
+    return self.map_items(movies)
   end
+
+  def self.map_items(movie_data)
+    return movie_data.map { |movie| Movie.new(movie)  }
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM movies
+    WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+
+    if result.count > 0
+      return Movie.map_items(result)
+    else
+      return nil
+    end
+  end
+
+
 
 end
